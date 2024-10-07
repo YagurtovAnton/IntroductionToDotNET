99fdb1c83ab59bc20f7275195b390a96710d0f8b
YagurtovAnton <Yagur_oo18@top-academy.ru>
2024-10-08 01:02:39 +0300

    ClassWork 05.10.24

---
+++ Array/Program.cs
@@ -0,0 +1,90 @@
+﻿#define ARRAYS_1
+#define ARRAYS_2
+//#define JAGED_ARRAY
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+#if ARRAYS_1
+			Console.WriteLine("Введите размер массива");
+			int n = Convert.ToInt32(Console.ReadLine());
+			//int[]arr = new[] { 3, 5, 8, 13, 21 };
+			int[] arr = new int[n];
+			Random rend = new Random(0);
+			for (int i = 0; i < arr.Length; i++)
+			{
+				arr[i] = rend.Next(100);
+			}
+			for (int i = 0; i < arr.Length; i++)
+			{
+				Console.Write(arr[i] + "\t");
+			}
+			Console.WriteLine();
+			foreach (int i in arr)//Range-based for
+			{
+				Console.Write(i + "\t");
+			}
+			Console.WriteLine();
+#endif
+#if ARRAYS_2
+			Console.Write("Введите количетсво строк:");
+			int rows = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Введите количество элементов строки: ");
+			int cols = Convert.ToInt32(Console.ReadLine());
+
+			int[,] i_arr_2 = new int[rows, cols];
+
+			Console.WriteLine(i_arr_2.Rank);
+
+			Random rand = new Random(0);
+			for (int i = 0; i < i_arr_2.GetLength(0); i++)
+			{
+				for (int j = 0; j < i_arr_2.GetLength(1); j++)
+				{
+					i_arr_2[i, j] = rand.Next(100);
+				}
+			}
+			for (int i = 0; i < i_arr_2.GetLength(0); i++)
+			{
+				for (int j = 0; j < i_arr_2.GetLength(1); j++)
+				{
+					Console.Write(i_arr_2[i, j] + "\t");
+				}
+				Console.WriteLine();
+			}
+			foreach (int i in i_arr_2)//Range-based for
+			{
+				Console.Write(i + "\t");
+			}
+			Console.WriteLine();
+#endif
+#if JAGED_ARRAY
+
+			int[][] arr_jagged = new int[][]
+			{   new int[] { 0, 1, 1, 2 },
+				new int[] { 3, 5, 8, 13, 21 },
+				new int[] { 34, 55, 89 },
+				new int[] { 144, 233, 377, 610 }
+			};
+			for (int i = 0; i < arr_jagged.Length; i++)
+			{
+				for (int j = 0;j< arr_jagged[i].Length; j++);
+				{
+					Console.Write(arr_jagged[i][j] + "\t");
+				}
+                Console.WriteLine();
+            }
+#endif
+
+		}
+	}
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Console.SetWindowSize(Console.WindowWidth, Console.WindowHeight);
+			Random rand = new Random();
+			int x=rand.Next(Console.WindowWidth);
+			int y= rand.Next(Console.WindowHeight);
+			Console.CursorVisible = false;
+			ConsoleKey key;
+			do
+			{
+				Console.Clear();
+				Console.SetCursorPosition(0,0);
+				Console.ResetColor();
+                Console.WriteLine($"X={x}\t Y={y}");
+                Console.SetCursorPosition(x, y);
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine((char)2);
+				key = Console.ReadKey(true).Key;
+				switch (key)
+
+				{
+					case ConsoleKey.UpArrow:
+					case ConsoleKey.W: y--; break;
+					case ConsoleKey.DownArrow:
+					case ConsoleKey.S: y++; break;
+					case ConsoleKey.LeftArrow:
+					case ConsoleKey.A: x--; break;
+					case ConsoleKey.RightArrow:
+					case ConsoleKey.D: x++; break;
+				}
+				if (
+					x == 0 || y == 0 || 
+					x == Console.WindowWidth || y == Console.WindowHeight
+					) Console.Beep();
+				if (x == 0) x = 1;
+				if (x == Console.WindowWidth) x = Console.WindowWidth - 1;
+				if (y == 0) y = 1;
+				if (y == Console.WindowHeight) y = Console.WindowHeight - 1;
+			} while (key != ConsoleKey.Escape);
+		}
+	}
+}
